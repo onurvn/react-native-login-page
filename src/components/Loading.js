4dda83f7661f8e5f8604b7d446bc62e6c981c5c8
Onur Avan <avanonur@hotmail.com>
2025-04-24 23:03:24 +0300

    loading components created and updated

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ActivityIndicator, Pressable } from 'react-native'
 import React from 'react'
 
-const Loading = () => {
+const Loading = ({ isLoading }) => {
     return (
-        <View>
-            <Text>Loading</Text>
+        <View style={styles.container}>
+            <Pressable
+                onPress={() => isLoading()}
+
+                style={[{}, styles.closeButtonContainer]}>
+                <Text style={styles.closeButton}>X</Text>
+            </Pressable>
+            <ActivityIndicator color="#0061FB" />
+            <Text style={styles.loginText}>Loading</Text>
         </View>
     )
 }
 
 export default Loading
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        position: "absolute",
+        backgroundColor: "#3E3F5B",
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    loginText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 20
+    },
+    closeButtonContainer: {
+        borderRadius: 99,
+        alignItems: "center",
+        justifyContent: "center",
+        height: 30,
+        width: 30,
+        position: "absolute",
+        top: 50,
+        right: 10
+    },
+    closeButton: {
+        color: "white",
+    },
+})
