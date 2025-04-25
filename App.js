c6ce4ac71466bc7d98745868d648087d81cdce98
Onur Avan <avanonur@hotmail.com>
2025-04-25 22:48:18 +0300

    app navigation

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import React from 'react'
-import LoginPage from './src/screens/LoginPage'
-import SignUpPage from './src/screens/SignUpPage'
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-const Stack = createNativeStackNavigator();
+import RootNavigation from './src/navigation/RootNavigation'
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-
-        <Stack.Screen component={LoginPage} name='Login' />
-        <Stack.Screen component={SignUpPage} name='SignUp' />
-
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+  return <RootNavigation />
 }
 
 export default App
