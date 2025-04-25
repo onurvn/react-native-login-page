0a1b3126b9223ca9648bf0af0c2592fe7f7f51a6
Onur Avan <avanonur@hotmail.com>
2025-04-25 22:45:58 +0300

    background color changed

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         position: "absolute",
-        backgroundColor: "#3E3F5B",
+        backgroundColor: "#123458",
         height: "100%",
         width: "100%",
         alignItems: "center",
