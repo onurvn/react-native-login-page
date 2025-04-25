77ba30b932c1df0ab6fb9cf399545800a0c250a5
Onur Avan <avanonur@hotmail.com>
2025-04-25 22:47:18 +0300

    home page and profile page added

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import SignUpPage from "./SignUpPage";
+
+export { HomePage, LoginPage, ProfilePage, SignUpPage }
