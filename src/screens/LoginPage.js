d320d73a3534b2700a2fa3aafe653ba8571142a3
Onur Avan <avanonur@hotmail.com>
2025-04-27 15:00:53 +0300

    fix bug

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -6,14 +6,13 @@ import { Loading, CustomTextInput, CustomButton } from '../components';
 const LoginPage = ({ navigation }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [result, setResult] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
     return (
         <View style={styles.container}>
             <StatusBar style="auto" />
 
-            <Text style={styles.welcome}>Welcome {result} </Text>
+            <Text style={styles.welcome}>Welcome</Text>
 
             <Image
                 style={styles.image}
