fe75dd8f9b5085e8e06da7a0b45205a66e8233ff
Onur Avan <avanonur@hotmail.com>
2025-04-29 00:37:15 +0300

    added extraReducers and user login

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,9 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+
+export const login = createAsyncThunk("user/login", async ({ email, password }) => {
+    try {
+        const auth = getAuth();
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const user = userCredential.user;
+        const token = user.stsTokenManager.accessToken;
+
+        const userData = {
+            token,
+            user: user
+        }
+
+        return userData
+
+    } catch (error) {
+        throw error
+    }
+})
 
 const initialState = {
     email: null,
     password: null,
     isLoading: false,
+    isAuth: false,
+    token: null,
+    user: null,
+    error: null
 }
 
 export const userSlice = createSlice({
@@ -20,6 +45,24 @@ export const userSlice = createSlice({
         setIsLoading: (state, action) => {
             state.isLoading = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(login.pending, (state) => {
+                state.isLoading = true;
+                state.isAuth = false;
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isAuth = true;
+                state.user = action.payload.user;
+                state.token = action.payload.token;
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isAuth = false;
+                state.error = action.error.message;
+            })
     }
 })
 
