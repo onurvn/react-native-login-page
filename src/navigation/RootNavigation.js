70751462d3e0d79276af998c4ddc54d6f07a68b8
Onur Avan <avanonur@hotmail.com>
2025-04-28 22:25:27 +0300

    fix

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import AuthStack from './AuthStack'
 import UserStack from './UserStack'
+import app from '../../firebaseConfig'
 
 const RootNavigation = () => {
     const isAuth = false;
